cc718c1f72048ac18921d7b47b128569960f48b8
Suriya <Suriya6374570440@gmail.com>
2024-11-02 19:16:27 +0530

    Update Footer.jsx

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -39,6 +39,11 @@ const Footer = () => {
       },
     },
   };
+  const LoadingSpinner = () => (
+    <div className="flex justify-center items-center h-32 w-32">
+      <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-white"></div>
+    </div>
+  );
 
   // Create dynamic socialLinks array using fetched data
   const socialLinks = [
